b7d9af8ff5ee4a2ac0900cf43e897bf345c77cd0
Sminth <emmanuelmalan@gmail.com>
2022-10-26 08:43:28 +0000

    maj

--- config/backpack/base.php
+++ config/backpack/base.php
@@ -71,7 +71,7 @@ return [
     // ------
 
     // Menu logo. You can replace this with an <img> tag if you have a logo.
-    'project_logo'   => '<b>Marthe&</b>Marie',
+    'project_logo'   => '<b>soutraitance</b>',
 
     // Show / hide breadcrumbs on admin panel pages.
     'breadcrumbs' => true,
